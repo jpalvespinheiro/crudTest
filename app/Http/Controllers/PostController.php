3bde85b3db4c332204443fdd47ec61672e5cda5f
Joao Pedro <jpalvespinheiro@gmail.com>
2023-08-10 09:15:17 -0300

    atualização

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,43 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-
-    public function index()
+    public function create(Request $request)
     {
-        //
+        $new_post = [
+            'title' => 'Meu primeiro crud',
+            'content' => 'Conteúdo Qualquer',
+            'author' => 'João Pedro'
+        ];
+
+        $post = new Post($new_post);
+        $post->save($new_post);
+
+        dd($new_post);
+    }
+    
+    public function read(int $id)
+    {
+        // $dados = $request->all();
+
+        $post = new Post();
+
+        $post = $post->find($id);
+
+        return $post;
+
     }
 
-    public function create()
+    public function all(Request $request)
     {
-        //
+        $posts = Post::all();
+
+        return $posts;
     }
 
-    public function store(Request $request)
+    public function update(Request $request)
     {
-        //
+        $posts = Post::where('id', '>', 1)->update([
+            'author' => 'Girlene',
+            'title' => 'Eu sou autora desse livro'
+        ]);
+
+        return $posts;
+    }
+    
+    public function delete(Request $request)
+    {
+        $posts = Post::where('id','>',0)->delete();
+
+        return $posts;
+
     }
 
-    public function show(string $id)
-    {
-        //
-    }
 
-    public function edit(string $id)
-    {
-        //
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/read', [PostController::class, 'read']);
+Route::get('/posts/read/{id}', [PostController::class, 'read']);
 Route::get('/posts/all', [PostController::class, 'all']);
 Route::get('/posts/update', [PostController::class, 'update']);
 Route::get('/posts/delete', [PostController::class, 'delete']);
